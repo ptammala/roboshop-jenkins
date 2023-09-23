c9996b20f94c219c2ebab8d9778c97d858d6cdef
github-repos <sheshankprakash260@gmail.com>
2023-09-23 13:47:13 +0530

    cipipeline groovy

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -2,7 +2,7 @@ def call() {
 
     node('workstation')
             {
-                sh "find . | sed -e '1d' |xarg rm -rf"
+                sh "find . | sed -e '1d' | xargs rm -rf"
                 git branch: 'main', url: "https://github.com/ptammala/${component}"
 
                 stage('Compile Code') {
