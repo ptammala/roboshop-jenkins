39993c54bfc5b83570f943dbf93c72e8f7e0b5e8
github-repos <sheshankprakash260@gmail.com>
2023-09-25 00:53:30 +0530

    cipipeline groovy

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -3,7 +3,18 @@ def call() {
     node('workstation')
             {
                 sh "find . | sed -e '1d' | xargs rm -rf"
-                git branch: "${BRANCH_NAME}", url: "https://github.com/ptammala/${component}"
+                //git branch: "${BRANCH_NAME}", url: "https://github.com/ptammala/${component}"
+                if (env.TAG_NAME ==~ ".*"){
+                    env.branch_name = "refs/tags/${TAG_NAME}"
+                }
+                else{
+                    env.branch_name = $"(env.BRANCH_NAME)"
+                }
+                checkout scmGit(
+                        branches: [[name: "refs/tags/v3.0"]],
+                        userRemoteConfigs: [[ url: "https://github.com/ptammala/${component}" ]]
+                )
+
 
                 stage('Compile Code') {
                     common.compile()
